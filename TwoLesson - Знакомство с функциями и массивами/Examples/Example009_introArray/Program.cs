c80f0c09e34379ec0c40fa444da794189d837212
sergey <fedor945@yandex.ru>
2022-10-03 15:55:26 +0300

    ex010 and 011

--- TwoLesson - Знакомство с функциями и массивами/Examples/Example009_introArray/Program.cs
+++ TwoLesson - Знакомство с функциями и массивами/Examples/Example009_introArray/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿int Max(int arg1, int arg2, int arg3) {
+    int result = arg1;
+    if(arg2 > result) result = arg2;
+    if(arg3 > result) result = arg3;
+    return result;
+}
+int[] array = {1, 2, 3, 31, 51, 5, 6, 523, 18 };
+
+
+// int a1 = 15, b1 = 21, c1 = 39, 
+//     a2 = 12, b2 = 23, c2 = 33, 
+//     a3 = 13, b3 = 23, c3 = 33;
+
+// int max1 = Max(a1, b1, c1); 
+// int max2 = Max(a2, b2, c2); 
+// int max3 = Max(a3, b3, c3); 
+
+int max = Max(
+    Max(array[0], array[1], array[2]), 
+    Max(array[3], array[4], array[5]), 
+    Max(array[6], array[7], array[8])); 
+    
+Console.WriteLine(max);
+
---
+++ TwoLesson - Знакомство с функциями и массивами/Examples/Example010_MethodArray/Program.cs
@@ -0,0 +1,16 @@
+﻿int[] array = {1, 2, 3, 31, 51, 5, 6, 523, 18 };
+
+int n = array.Length;
+int find = 4;
+
+int index = 0;
+
+while (index < n) 
+{
+    if(array[index] == find) 
+    {
+        Console.WriteLine(index);
+        break;
+    }
+    index++;
+}
